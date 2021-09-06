7de9cfbcd02fc01063b661b6fee559ad2348263e
Leticia Robledo <76557301+leticiarobledo@users.noreply.github.com>
2021-09-06 16:55:41 +0200

    Update libraries

--- nlp_chatbot.py
+++ nlp_chatbot.py
@@ -4,9 +4,8 @@ import pandas as pd
 import nltk 
 import numpy as np
 import re
-from nltk.stem import wordnet # to perform lemmitization
+from nltk.stem import wordnet # to perform lemmatization
 from sklearn.feature_extraction.text import CountVectorizer # to perform bow
-from sklearn.feature_extraction.text import TfidfVectorizer # to perform tfidf
 from nltk import pos_tag # for parts of speech
 from sklearn.metrics import pairwise_distances # to perfrom cosine similarity
 from nltk import word_tokenize # to create tokens
